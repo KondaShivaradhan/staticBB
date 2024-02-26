c9029e615ae4d644c65cfbac4c5ed3f3d4b5c5f3
Konda Shivaradhan <38292566+KondaShivaradhan@users.noreply.github.com>
2024-02-26 06:29:37 -0500

    new nav

--- script.js
+++ script.js
@@ -1,3 +1,44 @@
+function myFunction() {
+  small();
+}
+
+function small() {
+  $("#ntext").hide().css({
+    opacity: "1",
+  });
+  $("#navi").animate(
+    {
+      backgroundColor: "linear-gradient(145deg,white,black,white)",
+    },
+    1000
+  );
+  // document.getElementById('scrolldiv').style.display = 'none';
+  document.getElementById("small").style.display = "flex";
+  document.getElementById("svg").style.cssText =
+    "height:0%; width:0%;transition:all 0.3s";
+  document.getElementById("rot").style.transition = "all 0.5s";
+  document.getElementById("navi").style.height = "70px";
+  document.getElementById("rot").style.transform = "rotate(" + 90 + "deg)";
+}
+
+function big() {
+  console.log("big called");
+  // document.getElementById('scrolldiv').style.display = '';
+  document.getElementById("small").style.display = "none";
+  document.getElementById("svg").style.cssText = "height:50%; width:50%;";
+  $("#ntext").show();
+  document.getElementById("navi").style.height = "100%";
+  document.getElementById("rot").style.transform = "rotate(" + 270 + "deg)";
+}
+
+function navi() {
+  if (document.getElementById("navi").offsetHeight == "70") {
+    big();
+  } else {
+    small();
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   // Your code using document.getElementById here
 
